3bb31ee0ddfec2a6a924691ecce11c8692ee1142
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-05-12 01:50:20 +0300

    users docks v.0.1 done

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const { authRouter } = require('./routes');
 
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
@@ -13,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/users', authRouter);
 
 app.use((req, res) => {
--- routes/authApi.js
+++ routes/authApi.js
@@ -7,19 +7,25 @@ const { userValidation } = require('../schemas');
 const { authentication, upload } = require('../middlewares');
 const { validateBody } = require('../helpers');
 
-const { userRegisterShema, emailShema, userLoginShema } = userValidation;
+const { userRegisterShema, emailShema, userLoginShema, userUpdateShema } =
+  userValidation;
 
 router.post('/register', validateBody(userRegisterShema), authCtrl.register);
 
-router.get('/verify/:verifycationToken', authCtrl.verify);
+// router.get('/verify/:verifycationToken', authCtrl.verify);
 
-router.post('/verify', validateBody(emailShema), authCtrl.reVerify);
+// router.post('/verify', validateBody(emailShema), authCtrl.reVerify);
 
 router.post('/login', validateBody(userLoginShema), authCtrl.login);
 
 router.get('/current', authentication, authCtrl.current);
 
-router.patch('/update', authentication, authCtrl.update);
+router.patch(
+  '/update',
+  authentication,
+  validateBody(userUpdateShema),
+  authCtrl.update
+);
 
 router.post('/logout', authentication, authCtrl.logout);
 
