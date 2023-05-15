5e26bf24d69ac3bc71bce4ba2634d7ea2fc45725
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-05-15 20:30:54 +0300

    add populate pets and notices

--- controller/authController.js
+++ controller/authController.js
@@ -103,7 +103,7 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email }).populate('pets notices');
 
   if (!user) {
     throw httpError(401, `Email or password is wrong`);
